77cda92e432a1baabce01030873b70d503fef004
keeganthomp <whereiskeegan@gmail.com>
2017-07-13 16:46:16 -0500

    updating route names and path names to be consistint

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const dbUrl = "mongodb://localhost:27017/stats";
 
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var api = require('./routes/api');
 
 var app = express();
 
@@ -34,9 +34,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', index);
-app.use('/users', users);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,21 +43,6 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
-
-
-
-
-// error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
 
 
 app.listen(4000, function() {
--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var User = require("../models/user");
 var jwt = require("jsonwebtoken");
 var jwtConfig = require("../jwtConfig");
+var Stat = require("../models/stat");
+
 
 /* GET users listing. */
 router.get("/", function(req, res, next) {
@@ -47,6 +49,38 @@ router.post("/login", (req, res) => {
     } else {
       return res.send("Password or Username is not correct");
     }
+  });
+});
+
+router.get("/activities", (req, res) => {
+  Stat.find().then(foundStats => {
+    res.send(foundStats);
+  });
+});
+
+
+router.post("/activities", (req, res) => {
+  var statContent = req.body;
+  var newStat = new Stat(statContent);
+  newStat.save()
+    .then(addedStat => {
+      res.send(addedStat);
+    })
+    .then(err => {
+      res.send(err);
+    });
+});
+
+router.get("/activities/:id", (req, res) => {
+  Stat.find({_id: req.params.id}, {activity:1, _id:0}).then(foundStats => {
+    res.send(foundStats);
+  });
+});
+
+router.put("/activities/:id", (req, res) => {
+  Stat.updateOne({_id: req.params.id}, req.body)
+  .then(updatedStats => {
+    res.send(updatedStats);
   });
 });
 
